606d295c97e3b4cd9f4a6850dfb2492609efba2f
rasom <rasom@ukr.net>
2013-07-10 10:31:36 +0300

    init

---
+++ src/Qwer/Curl/Curl.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Qwer;
+
+class Curl
+{
+    public static function makeRequest($url, $data = null)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        if (!is_null($data)) {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        }
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return $response;
+    }
+}
